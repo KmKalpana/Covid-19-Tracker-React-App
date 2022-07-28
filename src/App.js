b4ee10875d8fc9847927ee2ef8b457ae94219036
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-07-28 07:59:20 +0530

    update build

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from "react";
 import CovidData from './Components/Covid';
 //import Error from "./Error";
 function App() {
-    return ( < CovidData / > );
+    return ( <CovidData/> );
 }
 export default App;
--- src/Components/Covid.jsx
+++ src/Components/Covid.jsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./covid.css";
 const CovidData = () => {
         const [country, setCountry] = useState('');
@@ -26,7 +26,7 @@ const CovidData = () => {
             setTodayRecovered(todayRecovered);
         };
         useEffect(() => {
-            fetch("https://disease.sh/v3/covid-19/countries")
+            fetch("https://disease.sh/v3/covid-19/countries/india")
                 .then((res) => res.json())
                 .then((data) => {
                     setData(data);
@@ -47,47 +47,37 @@ const CovidData = () => {
                     setData(data);
                 });
         };
-        return ( <
-            div className = "Card" >
-            <
-            h1 > Covid - 19 Tracker < /h1>  <
-            div className = "search" >
-            <
-            form onSubmit = { submit } >
-            <
-            input type = "text"
+        return (
+             <>
+            <div className = "Card" >
+            <h1 > Covid - 19 Tracker </h1>  
+            <div className = "search" >
+            <form onSubmit = { submit } >
+            <input type = "text"
             className = "search-bar"
             onChange = { inputEvent }
             placeholder = "Enter Country Name" / >
-            <
-            button type = "submit" > < svg stroke = "currentColor"
+            <button type = "submit" > <svg stroke = "currentColor"
             fill = "currentColor"
             viewBox = "0 0 1024 1024"
             height = "1.5em"
             width = "1.5em"
-            xmlns = "http://www.w3.org/2000/svg" >
-            <
-            path d = "M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" >
-            <
-            /path> < /
-            svg > < /button > < /
-            form > < /
-            div > <
-            div className = "input_Info" >
-            <
-            p > Country Name: { country } < /p> <
-            p > Cases: { cases } < /p> <
-            p > Deaths: { deaths } < /p> <
-            p > Recovered: { recovered } < /p> <
-            p > Cases Today: { todayCases } < /p> <
-            p > Deaths Today: { todayDeaths } < /p> <
-            p > Recovered Today: { todayRecovered } < /p> <
-            p > < /p>
-
-            <
-            /div>
-
-            <
-            /div> );
+            xmlns = "http://www.w3.org/2000/svg">
+            <path d = "M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" >
+            </path></svg>
+            </button > </form >
+             </div> 
+             <div className = "input_Info">
+            <p> Country Name: { country } </p> 
+            <p> Cases: { cases } </p> 
+            <p> Deaths: { deaths } </p> 
+            <p> Recovered: { recovered } </p>
+            <p> Cases Today: { todayCases } </p> 
+            <p> Deaths Today: { todayDeaths } </p> 
+            <p> Recovered Today: { todayRecovered } </p> 
+            <p> </p>
+            </div>
+            </div> 
+            </>);
         }
         export default CovidData;
